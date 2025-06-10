202b7f9b1f7ccc180c4929a5806d38504582f04f
yira <yira@yiras-MacBook-Pro.local>
2025-06-10 10:31:49 -0600

    fix logo pdf

--- app/cotizador/page.js
+++ app/cotizador/page.js
@@ -160,7 +160,7 @@ export default function Cotizador() {
     let y = 15;
     const pageWidth = doc.internal.pageSize.getWidth();
     // Logo
-    doc.addImage("/logo-azul.png", "PNG", 10, 5, 40, 22.5);
+    doc.addImage("/logo-azul.png", "PNG", 10, -6, 40, 38);
     // Title
     doc.setFontSize(16);
     doc.setFont("helvetica", "bold");
